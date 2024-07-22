c0533d493fa351c7910c925871391ddbf6397f48
StanAdim <stanjustine@gmail.com>
2024-07-22 17:42:09 +0300

    Change concept note

--- components/aboutConference.vue
+++ components/aboutConference.vue
@@ -6,7 +6,11 @@
 // }
 // retriveSitedata()
 const getSitedData = ref({
-    aboutConference1: `The 8th Tanzania Annual ICT Conference 2024 (TAIC-2024), first Africa Edition, organized by the ICT Commission (ICTC), in collaboration with ELEVATE and the African Union Development Agency-NEPAD (AUDA-NEPAD), is scheduled to take place in Dar es Salaam, Tanzania at the Julius Nyerere International Conference Centre (JNICC) from October 13th to October 17th, 2024. Pre-conference events, including the African AI Competition, are planned for October 13th and 14th, and will be open to public participation.`,
+    aboutConference1: `The 8th Tanzania Annual ICT Conference 2024 (TAIC-2024), first Africa Edition, organized by the
+     ICT Commission (ICTC), in collaboration with ELEVATE and the African Union Development
+     Agency-NEPAD (AUDA-NEPAD), is scheduled to take place in Dar es Salaam, Tanzania at the Julius Nyerere International
+     Conference Centre (JNICC) from October 13th to October 17th, 2024. Pre-conference events, including the African AI Competition,
+     are planned for October 13th and 14th, and will be open to public participation.`,
   aboutConference2: `Building on the success of previous conferences, TAIC-2024 will gather a broad spectrum of participants,
   including ICT professionals, practitioners, academics, researchers, development partners, industry leaders, and innovators in digital technology.
   This year’s theme, “Unleashing the Power of Artificial Intelligence and Robotics for Socio-economic Transformation”, focuses on the transformative
